8e460a27bf5694ef6a44577f23775a65323d5f2f
CristianR <cristiandramirezmm.2024@gmail.com>
2020-06-29 19:12:01 -0500

    nuevas funciones

--- rsa/Funciones/__init__.py
+++ rsa/Funciones/__init__.py
@@ -3,17 +3,14 @@
 
 def max_min(a,b):
     if a > b:
-        p = a
-        m = b
-    else:
-        p = b
-        m = a
-    return [p,m];
+        return [a, b]
+    return [b,a]
+
 
 def alg_euclides_extendido(a, b):
 
     #primero se ordenan los dos valores del mayor al menor#
-    ord = max_min(a,b);
+    ord = max_min(a,b)
 
     #los primeros datos en los arreglos siempre seran los mismos#
     pri=[ord[0],0,1,0]
@@ -57,7 +54,31 @@ def alg_euclides_extendido(a, b):
     else:
         return [seg[0],seg[2],seg[3]]
 
+# si bien el algoritmo extendido de euclides puede hacer el calculo del mcd, es menos eficiente ya que ademas de este calcula otros valores, si no son necesarios en la operacion
+# a realizar, es preferible usar la version especifica del mcd, tambien conocido como "algoritmo de euclides"
 
 
-val= True
+def mcd(a,b):
+    if b==0:
+        return a
+    else:
+        return mcd(b,a%b)
+
+def fi(a,b):
+    return (a-1)*(b-1)
+
+def indicatriz_euler(n):
+    # usare j para definir el valor minimo para el coprimo del valor
+    j=n//10%100;
+    for i in range(j,n):
+        if mcd(i,n)==1:
+
+            return i;
+
+
+
+
+
 print(alg_euclides_extendido(300,55))
+print(mcd(97,300))
+print(indicatriz_euler(40))
