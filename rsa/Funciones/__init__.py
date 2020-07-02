7f553e44010935a4e279cd66028397fecc4e4905
CristianR <cristiandramirezmm.2024@gmail.com>
2020-07-01 22:19:32 -0500

    termine el algoritmo de ecnriptacion y desencriptacion RSA

--- rsa/Funciones/__init__.py
+++ rsa/Funciones/__init__.py
@@ -1,3 +1,4 @@
+import random
 
 
 
@@ -69,17 +70,177 @@ def fi(a,b):
 
 def indicatriz_euler(n):
     # usare j para definir el valor minimo para el coprimo del valor
-    j=n//10%100;
-    for i in range(j,n):
-        if mcd(i,n)==1:
+    #esta funcion se puede mejorar
 
-            return i;
+    #j=n//10%100
+    while True:
+        i=get_primo_2(20,50)
+        if mcd(i,n)==1:
+            return i
+
 
 def inversa(n,mod):
-    
+    ord=alg_euclides_extendido(n,mod)
+    if ord[0]==1:
+        if ord[2]<0:
+            return ord[2]+mod
+        return ord[2]
+    return 0
+
+def val_n(p,q):
+    return p*q
+
+
+def val_d(e,p,q):
+    valFi = fi(p,q)
+    return 1
 
 
 
-print(alg_euclides_extendido(300,55))
-print(mcd(97,300))
-print(indicatriz_euler(40))
+
+
+
+
+
+def is_primo(a):
+    if a%2==0:
+        return False
+    for i in range(3,a//2,2):
+        if a%i==0:
+            return False
+    return True
+
+#deseo generar primos aleatorios
+def get_primo(fro_m):
+    i=random.randrange(fro_m,1000)
+    while True:
+        if is_primo(i):
+            return i
+        i=i+1
+
+def get_primo_2(fro_m,top):
+    i=random.randrange(fro_m,top)
+    while True:
+        if is_primo(i):
+            return i
+        i=i+1
+
+def get_publica(p,q):
+    n=val_n(p,q)
+    fi_1=fi(p,q)
+    e=indicatriz_euler(fi_1)
+
+    return [e,n]
+
+def get_privada(e,p,q):
+    d=inversa(e,fi(p,q))
+    return [d,val_n(p,q)]
+
+
+def comprobar(e,p,q):
+
+    fi_1=fi(p,q)
+
+    i=0
+    j=0
+    v=True
+    while(v):
+        i=i+1
+        j=(1+(i*fi_1))/e
+        if(j%1==0):
+            v=False
+
+    return [i,j]
+
+def int_char(char):
+    c=ord(char)
+    if 65<=c<=90:
+        return c-64
+    elif 97<=c<=122:
+        return c-96
+    return 0
+
+def char_int(int):
+    if int==0:
+        return '.'
+    return chr(int+96)
+
+def binario(e):
+    j=[]
+    return 1
+
+
+def encriptar(mensaje,e,n):
+    bi=str(bin(e))[2:]
+    j=[]
+
+
+    for i in range(len(mensaje)):
+        acum=1
+        h=0
+
+        for k in reversed(range(len(bi))):
+            if bi[k] == '1':
+               acum=acum*(int_char(mensaje[i])**(2**h)%n)
+
+            h=h+1
+
+        j.append(acum%n)
+
+
+    return j
+
+
+def desencriptar(mensaje,d,n):
+    bi=str(bin(d))[2:]
+    j=[]
+
+    for i in range(len(mensaje)):
+
+        acum=1
+        h=0
+        for k in reversed(range(len(bi))):
+            if bi[k]=='1':
+                acum=acum*(mensaje[i]**(2**h)%n)
+            h=h+1
+
+
+        j.append(char_int(acum%n))
+
+    return j
+
+
+
+
+
+
+
+print(ord('A'))
+print(ord('Z'))
+print(ord('a'))
+print(ord('z'))
+
+#print(encriptar("mensaje"))
+
+print(str(bin(7))[2:])
+
+p=get_primo(200)
+q=get_primo(201)
+
+r=get_publica(p,q)
+
+j=get_privada(r[0],p,q)
+
+print(inversa(j[0],fi(p,q)))
+print(r[0],r[1])
+
+k=encriptar("hola.mundo.",r[0],r[1])
+h=desencriptar(k,j[0],j[1])
+print(encriptar("hola.mundo.",r[0],r[1]))
+print(h)
+
+
+
+
+print("".join(h))
+
